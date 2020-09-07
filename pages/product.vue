0340bd3ef0b12257e86eaf960ac0c9e4e56564bd
Christian Zigah <efochristian@outlook.com>
2020-09-07 10:56:11 +0000

    last commit

---
+++ pages/product.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <div class="a-section">
+            <div class="a-spacing-large"></div>
+            <h2 style="text-align: center">Add a new Product</h2>
+          </div>
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
